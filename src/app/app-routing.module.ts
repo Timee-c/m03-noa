8d0ca1a5ec475c364c31c73279e9d97450390a5b
jdoscottini <jonathan.scottini@philips.com>
2024-06-13 09:58:33 -0300

    Add dashboard page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { DashboardMainComponent } from './dashboard/dashboard-main.component';
 
 const routes: Routes = [
- // {path: "", component: DashboardComponent}
+  {path: "dashboard", component: DashboardMainComponent},
   {path:"group",component: FooterComponent}
 ];
 
