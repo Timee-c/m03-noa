b7d73e70d3a486a96b3b7188cee3ae525361f38e
Guilherme Guimaraes <guilherme.guimaraes@philips.com>
2024-06-13 14:18:49 -0300

    add tela produto grupo

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./group.component.css']
 })
 export class GroupComponent {
-
+  groups = [
+    { id: 1, name: 'Group 1', description: 'Description 1' },
+    { id: 2, name: 'Group 2', description: 'Description 2' },
+    // Add more group objects as needed
+  ];
 }
